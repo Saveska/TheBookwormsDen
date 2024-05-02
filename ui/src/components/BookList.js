5b8ca2b2620c6486dd0a3956a0108cd2bbb1b8d0
DESKTOP-UB16K1F\Amala Solutions <Petar T>
2024-05-02 16:57:09 +0200

    Added pagination buttons.

--- ui/src/components/BookList.js
+++ ui/src/components/BookList.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import BookCard from './BookCard';
-import booksData from '../data/booksData';
+import React, { useState, useEffect } from "react";
+import BookCard from "./BookCard";
+import booksData from "../data/booksData";
+import PaginationButtons from "./Home/PaginationButtons";
 
 const BookList = ({ category, search }) => {
-    const filteredBooks = booksData.filter(book => {
-        return (
-            book.category.includes(category) &&
-            book.title.toLowerCase().includes(search.toLowerCase())
-        );
-    });
+	const [currentPage, setCurrentPage] = useState(1);
+	const booksPerPage = 9;
+	const [filteredBooks, setFilteredBooks] = useState([]);
 
-    return (
-        <div className="grid grid-cols-4 gap-5">
-            {filteredBooks.length > 0 ?
-                filteredBooks.map(book => (
-                    <BookCard key={book.id} book={book} />
-                ))
-            :
-                <div className="text-white">No <span className="font-bold">{category}</span> books found :(</div>
-            }
-        </div>
-    );
+	useEffect(() => {
+		const newFilteredBooks = booksData.filter((book) => {
+			return (
+				book.category.includes(category) &&
+				book.title.toLowerCase().includes(search.toLowerCase())
+			);
+		});
+		setFilteredBooks(newFilteredBooks);
+		setCurrentPage(1);
+	}, [category, search]);
+
+	const indexOfLastBook = currentPage * booksPerPage;
+	const indexOfFirstBook = indexOfLastBook - booksPerPage;
+	const currentBooks = filteredBooks.slice(indexOfFirstBook, indexOfLastBook);
+
+	const nextPage = () => {
+		setCurrentPage(currentPage + 1);
+	};
+
+	const prevPage = () => {
+		setCurrentPage(currentPage - 1);
+	};
+
+	return (
+		<div className='flex flex-col justify-between w-full '>
+			<div className='grid h-full grid-cols-3 row-span-3 gap-5'>
+				{currentBooks.length > 0 ? (
+					currentBooks.map((book) => <BookCard key={book.id} book={book} />)
+				) : (
+					<div className='text-white'>
+						No <span className='font-bold'>{category}</span> books found :(
+					</div>
+				)}
+			</div>
+			<div className='row-span-1'>
+				<PaginationButtons
+					currentPage={currentPage}
+					setCurrentPage={setCurrentPage}
+					indexOfLastBook={indexOfLastBook}
+					filteredBooks={filteredBooks}
+					nextPage={nextPage}
+					prevPage={prevPage}
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default BookList;
--- ui/src/data/booksData.js
+++ ui/src/data/booksData.js
@@ -46,7 +46,148 @@ const booksData = [
         title: "The Subtle Art of Not Giving a F*ck",
         author: "Mark Manson",
         category: "Non-fiction"
+    },
+    {
+        id: 9,
+        title: "The Catcher in the Rye",
+        author: "J.D. Salinger",
+        category: "Fiction"
+    },
+    {
+        id: 10,
+        title: "The Hobbit",
+        author: "J.R.R. Tolkien",
+        category: "Fiction"
+    },
+    {
+        id: 11,
+        title: "The Alchemist",
+        author: "Paulo Coelho",
+        category: "Fiction"
+    },
+    {
+        id: 12,
+        title: "Crime and Punishment",
+        author: "Fyodor Dostoevsky",
+        category: "Fiction"
+    },
+    {
+        id: 13,
+        title: "Moby-Dick",
+        author: "Herman Melville",
+        category: "Fiction"
+    },
+    {
+        id: 14,
+        title: "The Odyssey",
+        author: "Homer",
+        category: "Fiction"
+    },
+    {
+        id: 15,
+        title: "The Lord of the Rings",
+        author: "J.R.R. Tolkien",
+        category: "Fiction"
+    },
+    {
+        id: 16,
+        title: "Brave New World",
+        author: "Aldous Huxley",
+        category: "Fiction"
+    },
+    {
+        id: 17,
+        title: "The Picture of Dorian Gray",
+        author: "Oscar Wilde",
+        category: "Fiction"
+    },
+    {
+        id: 18,
+        title: "Anna Karenina",
+        author: "Leo Tolstoy",
+        category: "Fiction"
+    },
+    {
+        id: 19,
+        title: "The Brothers Karamazov",
+        author: "Fyodor Dostoevsky",
+        category: "Fiction"
+    },
+    {
+        id: 20,
+        title: "The Count of Monte Cristo",
+        author: "Alexandre Dumas",
+        category: "Fiction"
+    },
+    {
+        id: 21,
+        title: "Frankenstein",
+        author: "Mary Shelley",
+        category: "Fiction"
+    },
+    {
+        id: 22,
+        title: "The Adventures of Huckleberry Finn",
+        author: "Mark Twain",
+        category: "Fiction"
+    },
+    {
+        id: 23,
+        title: "War and Peace",
+        author: "Leo Tolstoy",
+        category: "Fiction"
+    },
+    {
+        id: 24,
+        title: "The Road",
+        author: "Cormac McCarthy",
+        category: "Fiction"
+    },
+    {
+        id: 25,
+        title: "The Stranger",
+        author: "Albert Camus",
+        category: "Fiction"
+    },
+    {
+        id: 26,
+        title: "One Hundred Years of Solitude",
+        author: "Gabriel García Márquez",
+        category: "Fiction"
+    },
+    {
+        id: 27,
+        title: "The Bell Jar",
+        author: "Sylvia Plath",
+        category: "Fiction"
+    },
+    {
+        id: 28,
+        title: "The Road Less Traveled",
+        author: "M. Scott Peck",
+        category: "Non-fiction"
+    },
+    {
+        id: 29,
+        title: "Man's Search for Meaning",
+        author: "Viktor E. Frankl",
+        category: "Non-fiction"
+    },
+    {
+        id: 30,
+        title: "Thinking, Fast and Slow",
+        author: "Daniel Kahneman",
+        category: "Non-fiction"
+    },
+    {
+        id: 31,
+        title: "The Power of Habit",
+        author: "Charles Duhigg",
+        category: "Non-fiction"
     }
+    
+    
+
 ];
 
 export default booksData;
