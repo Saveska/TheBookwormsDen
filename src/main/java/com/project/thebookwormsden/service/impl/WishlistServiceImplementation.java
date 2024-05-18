29264393193c5136e9edcf1876d5caa556d83583
Bojan Bekrikj <bojanbekrik@gmail.com>
2024-05-18 09:01:00 +0200

    Added methods add and remove from wishlist.

--- src/main/java/com/project/thebookwormsden/service/impl/WishlistServiceImplementation.java
+++ src/main/java/com/project/thebookwormsden/service/impl/WishlistServiceImplementation.java
@@ -1,6 +1,9 @@
 package com.project.thebookwormsden.service.impl;
 
+import com.project.thebookwormsden.model.Article;
+import com.project.thebookwormsden.model.User;
 import com.project.thebookwormsden.model.Wishlist;
+import com.project.thebookwormsden.repository.ArticleRepository;
 import com.project.thebookwormsden.repository.UserRepository;
 import com.project.thebookwormsden.repository.WishlistRepository;
 import com.project.thebookwormsden.service.WishlistService;
@@ -12,14 +15,41 @@ import java.util.List;
 public class WishlistServiceImplementation implements WishlistService {
 
     private UserRepository userRepository;
+    private ArticleRepository articleRepository;
+    private final WishlistRepository wishlistRepository;
 
-    public WishlistServiceImplementation(UserRepository userRepository) {
+    public WishlistServiceImplementation(UserRepository userRepository,
+                                         WishlistRepository wishlistRepository) {
         this.userRepository = userRepository;
+        this.wishlistRepository = wishlistRepository;
     }
 
     @Override
     public Wishlist getWishlistForUser(Long userId) {
         return userRepository.findByUserId(userId);
         // ovoj metod treba da se implementira vo user servisite
+    }
+
+    @Override
+    public void removeArticleFromWishlist(Long userId, Long articleId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("User not found"));
+        Wishlist wishlist = wishlistRepository.findWishlistByUser(user);
+        if (wishlist != null) {
+            Article articleToRemove = articleRepository.findById(articleId).orElseThrow(() -> new IllegalArgumentException("Article not found"));
+            wishlist.getArticlesList().remove(articleToRemove);
+            wishlistRepository.save(wishlist);
+        }
+    }
+
+    @Override
+    public void addArticleToWishlist(Long userId, Long articleId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("User not found"));
+        Wishlist wishlist = wishlistRepository.findByUser(user);
+        // ovoj metod treba da se implementira vo user servisite
+        if (wishlist != null) {
+            Article articleToAdd = articleRepository.findById(articleId).orElseThrow(() -> new IllegalArgumentException("Article not found"));
+            wishlist.getArticlesList().add(articleToAdd);
+            wishlistRepository.save(wishlist);
+        }
     }
 }
--- src/main/java/com/project/thebookwormsden/web/WishlistController.java
+++ src/main/java/com/project/thebookwormsden/web/WishlistController.java
@@ -3,10 +3,7 @@ package com.project.thebookwormsden.web;
 import com.project.thebookwormsden.model.Wishlist;
 import com.project.thebookwormsden.service.WishlistService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/wishlist")
@@ -18,7 +15,7 @@ public class WishlistController {
         this.wishlistService = wishlistService;
     }
 
-    @GetMapping("/wishlist/{userId}")
+    @GetMapping("/forUser/{userId}")
     public ResponseEntity<Wishlist> getWishlistForUser(@PathVariable Long userId) {
         Wishlist wishlist = wishlistService.getWishlistForUser(userId);
         if (wishlist != null) {
@@ -27,4 +24,26 @@ public class WishlistController {
             return ResponseEntity.notFound().build();
         }
     }
+
+    @DeleteMapping("/forUser/{userId}/article/{articleId}")
+    public ResponseEntity<Void> removeArticleFromWishlist(@PathVariable Long userId, @PathVariable Long articleId) {
+        try {
+            wishlistService.removeArticleFromWishlist(userId, articleId);
+            return ResponseEntity.noContent().build();
+        }
+        catch (IllegalArgumentException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/forUser/{userId}/articleToAdd/{articleId}")
+    public ResponseEntity<Void> addArticleToWishlist(@PathVariable Long userId, @PathVariable Long articleId) {
+        try {
+            wishlistService.addArticleToWishlist(userId, articleId);
+            return ResponseEntity.ok().build();
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 }
